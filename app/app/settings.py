ae9f0d836a5bf42e3a4b6a9aca15004dc9b2454e
Jon Covington <jon.covington@gmail.com>
2021-05-13 22:40:49 -0600

    Created core app and custom user model with tests.

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+# Custom User Model
+AUTH_USER_MODEL = 'core.CustomUser'
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = 'test@workbound.info'
+        password = 'testpass123'
+        User = get_user_model()
+        user = User.objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        """Tests that a superuser can be created"""
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', is_superuser=False)
+
+    def test_new_user_email_normalized(self):
+        """Test that user email is normalized"""
+        email = 'test@WORKBOUND.INFO'
+        user = get_user_model().objects.create_user(email, 'testpass123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpass123')
