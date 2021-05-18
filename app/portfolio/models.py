2b263ee85e199ebfdb422edc9cacf47eb2603068
Jon Covington <jon.covington@gmail.com>
2021-05-17 18:49:03 -0600

    Added foreign key of user to Portfolio model

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 
 class Portfolio(models.Model):
     reference = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(get_user_model(), models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
 
     def __str__(self) -> str:
         return self.reference
