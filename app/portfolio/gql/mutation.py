2de5378f0309811ce64fa97ee033d24768cde0e4
Jon Covington <jon.covington@gmail.com>
2021-05-28 15:21:08 -0600

    Clean up unused imports

--- app/portfolio/gql/mutation.py
+++ app/portfolio/gql/mutation.py
@@ -1,5 +1,3 @@
-from django.contrib.auth import get_user_model
-
 from graphql_jwt.decorators import permission_required
 from graphene import (Mutation,
                       Field,
@@ -8,32 +6,9 @@ from graphene import (Mutation,
                       DateTime,
                       )
 
-
 from portfolio.gql.types import CategoryType, TaskType
 from portfolio.models import Category, Task
 from portfolio.serializers import CategorySerializer, TaskSerializer
-
-User = get_user_model()
-
-
-# class CreateTask(Mutation):
-#     """Graphql mutation to create a Task"""
-#     class Arguments:
-#         title = String(required=True)
-#         description = String(required=True)
-#         duration = Int(required=True)
-#         created_by_id = Int(required=True)
-
-#     task = Field(TaskType)
-
-#     @classmethod
-#     @permission_required('portfolio.add_task')
-#     def mutate(cls, root, info, **kwargs):
-#         user_id = kwargs.pop('created_by_id')
-#         user = User.objects.get(id=user_id)
-#         task = Task.objects.create(created_by=user, **kwargs)
-#         task.save()
-#         return cls(task=task)
 
 
 class CreateTask(Mutation):
