0374b587cb367420bb77d54824293abeb4dc5643
Jon Covington <jon.covington@gmail.com>
2021-08-09 16:43:17 -0600

    updated entrypoint and separated firebase config into individual env properties

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 python manage.py migrate --noinput || exit 1
 python manage.py init_admin || exit 1
 python manage.py collectstatic --noinput || exit 1
+python manage.py wait_for_db
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
