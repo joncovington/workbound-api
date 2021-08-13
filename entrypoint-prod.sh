00a4e0239948d700e75f29305afe1f110e27b24c
Jon Covington <jon.covington@gmail.com>
2021-08-13 10:55:26 -0600

    nginx config change to stay on port 8000

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+python manage.py wait_for_db
 python manage.py migrate --noinput || exit 1
 python manage.py init_admin || exit 1
 python manage.py collectstatic --noinput || exit 1
-python manage.py wait_for_db
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
